d00c285f78a0f0062e48511bb169d47324fce52b
Mike Chan <mikekitckchan@gmail.com>
2018-12-17 22:01:06 +0800

    added threading

--- movie.py
+++ movie.py
@@ -1,19 +1,66 @@
 from bs4 import BeautifulSoup
 import requests
 from rottentomatoes import MovieScore
+import threading
+import time
 
-movies = []
-movies_scores = []
-data = requests.get("https://nowplayer.now.com/ondemand/seeall?filterType=appPlayable&nodeId=C201008200000129")
-content = data.text
-soup = BeautifulSoup(content, "lxml")
-titles = soup.find_all('p', attrs={'class' : 'title'})
+class NowMovieList(object):
+	def __init__(self):
+		print("Instantiating........")
+		self.link = "https://nowplayer.now.com/ondemand/seeall?filterType=appPlayable&nodeId=C201008200000129"
+		self.movies = []
+		self.movies_scores = []
 
-for title in titles:
-	movies.append(title.text)
+	def get_movies_name(self):
+		print("getting Now Movie List.......")
+		data = requests.get(self.link)
+		content = data.text
+		soup = BeautifulSoup(content, "lxml")
+		titles = soup.find_all('p', attrs={'class' : 'title'})
+		for title in titles:
+			self.movies.append(title.text)
 
-for item in movies:
-	print("reading "+item)
-	NewMovie = MovieScore(item)
-	NewMovie.run()
+	def get_movies_scores(self, movie_name):
+		NewMovie = MovieScore(movie_name)
+		NewMovie.run()
+		if NewMovie.result == 1:
+			self.movies_scores.append(NewMovie)
+		
+
+	def make_score_list(self):
+		print("getting movie score.............")
+		threads =[]
+		for item in self.movies:
+			t = threading.Thread(target=self.get_movies_scores, args=[item,])
+			threads.append(t)
+			t.start()
+			time.sleep(0.03)
+
+		main_thread = threading.currentThread()
+		for t in threading.enumerate():
+			if t is main_thread:
+				continue
+			t.join()
+
+		self.movies_scores.sort(key=lambda x: x.my_score, reverse=True)
+
+
+	def print_movies_scores(self):
+		print("printing reuslt.......")
+		for item in self.movies_scores:
+			print("name: "+item.name)
+			print("tomatoter score: "+item.tomatoter_score)
+			print("number of reviews: "+item.tomatoter_count)
+			print("Audience Score: "+item.audience_score)
+			print("----------------")
+
+	def run(self):
+		self.get_movies_name()
+		self.make_score_list()
+		self.print_movies_scores()
+
+if __name__ == '__main__':
+	new_list = NowMovieList()
+	new_list.run()
+
 	
--- rottentomatoes.py
+++ rottentomatoes.py
@@ -10,8 +10,11 @@ class MovieScore(object):
 		self.tomatoter_count = ""
 		self.audience_score = ""
 		self.tomatoter_link = ""
+		self.result = 1
+		self.my_score = 0
 
 	def search(self):
+		'''
 		link_movie_name = re.sub(r"\s+", '+', self.name)
 		link = "https://hk.search.yahoo.com/search?p="+link_movie_name+"+rotten+tomatoes+score"
 		data = requests.get(link)
@@ -21,51 +24,84 @@ class MovieScore(object):
 		title = str(title)
 		title = title.split('"')
 		self.tomatoter_link = title[5]
+		'''
+
+		find_movie_name = self.name
+	
+		'''if there are movies like XXX (full version), we just need XXX. So, stop reading when it read ('''
+		find_movie_name = find_movie_name.split('(', 1)[0]
+	
+		'''change input to all lower case, remove punctuation, and replace whitespace by _'''
+		find_movie_name = find_movie_name.lower()
+		find_movie_name = re.sub(r"[,?!:']", '', find_movie_name)
+		find_movie_name = re.sub(r"\s+", '_', find_movie_name)
+
+		'''join it with link of rottentomatoes'''
+		link = "https://www.rottentomatoes.com/m/"+find_movie_name+"_2018"
+		data = requests.get(link)
+
+		'''if no such movie, try _2018 or _2017, it is rottentomatoes rule'''
+		if data.status_code == 200:
+			self.tomatoter_link = link
+		else:
+			link = link.replace("_2018", "_2017")
+			data = requests.get(link)
+			if data.status_code == 200:
+				self.tomatoter_link = link
+			else:
+				link = link.replace("_2017", "")
+				data = requests.get(link)
+				if data.status_code == 200:
+					self.tomatoter_link = link
+				else:
+					self.tomatoter_link = "not found"
+					self.result = 0
 	
 	def rottemtomatoes_score(self):
-		data = requests.get(self.tomatoter_link)
-		if data.status_code == 200:
-			content = data.text
-			soup = BeautifulSoup(content, "lxml")
-			tomatoter = soup.find(class_ = "tomato-left")
+		if self.tomatoter_link != "not found":
+			data = requests.get(self.tomatoter_link)
+			if data.status_code == 200:
+				content = data.text
+				soup = BeautifulSoup(content, "lxml")
+				tomatoter = soup.find(class_ = "tomato-left")
 			
-			tomatoter_name = soup.find(('h1', {'class' : 'title hidden-xs no-trailer-title'}))
-			if tomatoter_name:
-				tomatoter_name = tomatoter_name.text
-				tomatoter_name = tomatoter_name.strip(' \t\n\r')
-				self.tomatoter_name = tomatoter_name
-			else:
-				self.tomatoter_name = "not found"
+				tomatoter_name = soup.find(('h1', {'class' : 'title hidden-xs no-trailer-title'}))
+				if tomatoter_name:
+					tomatoter_name = tomatoter_name.text
+					tomatoter_name = tomatoter_name.strip(' \t\n\r')
+					self.tomatoter_name = tomatoter_name
+				else:
+					self.tomatoter_name = "not found"
 			
-			tomatoter_count = tomatoter.find_all(class_ = "superPageFontColor")
-			if tomatoter_count:
-				tomatoter_count = tomatoter_count[3]
-				tomatoter_count = tomatoter_count.text
-				tomatoter_count = tomatoter_count [17:]
-				self.tomatoter_count = tomatoter_count.strip(' \t\n\r')
-			else:
-				self.tomatoter_count = "no tomatoter reviews"
+				tomatoter_count = tomatoter.find_all(class_ = "superPageFontColor")
+				if tomatoter_count:
+					tomatoter_count = tomatoter_count[3]
+					tomatoter_count = tomatoter_count.text
+					tomatoter_count = tomatoter_count [17:]
+					self.tomatoter_count = tomatoter_count.strip(' \t\n\r')
+				else:
+					self.tomatoter_count = "no tomatoter reviews"
 			
-			tomatoter_score = tomatoter.find(class_ = "critic-score meter")
-			if tomatoter_score:
-				tomatoter_score = tomatoter_score.text
-				self.tomatoter_score = tomatoter_score.strip(' \t\n\r')
-			else:
-				self.tomatoter_score = "no tomatoter score"
+				tomatoter_score = tomatoter.find(class_ = "critic-score meter")
+				if tomatoter_score:
+					tomatoter_score = tomatoter_score.text
+					self.tomatoter_score = tomatoter_score.strip(' \t\n\r')
+					self.my_score = int(self.tomatoter_score.strip('%'))
+				else:
+					self.tomatoter_score = "no tomatoter score"
 
-			audience_score = soup.find(class_ = "audience-score meter")
-			if audience_score:
-				audience_score = audience_score.text
-				audience_score= audience_score.strip(' \t\n\r')
-				self.audience_score = audience_score[0:3]
+				audience_score = soup.find(class_ = "audience-score meter")
+				if audience_score:
+					audience_score = audience_score.text
+					audience_score= audience_score.strip(' \t\n\r')
+					self.audience_score = audience_score[0:3]
+				else:
+					self.audience_score = "No audience score"
 			else:
-				self.audience_score = "No audience score"
-			print("Newmovie name: " + self.name)
-			print(" rotten name: " + self.tomatoter_name) 
-			print(" rotten score: " + self.tomatoter_score + " no. of review: " + self.tomatoter_count)
-			print("---")
+				self.result = 0
 		else:
-			print(self.name + "not found")
+			self.result = 0
+
 
 	def run(self):
 		self.search()
